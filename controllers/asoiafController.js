2d413b07ea1ef50fbd6c49e2bc4677c013adff02
DanielRenard <danieljuderenard@gmail.com>
2024-02-22 19:17:53 -0600

    pulling data from api

--- controllers/asoiafController.js
+++ controllers/asoiafController.js
@@ -3,16 +3,17 @@ const Models = require("../models");
 
 const axios = require('axios')
 
-const getAsoiaf = async (req, res) => {
+const getAsoiafs = async (req, res) => {
     await axios
     .get("https://anapioficeandfire.com/api/houses/")
     .then((response) => {
-      console.log(response)
-      res.status(200).send({ result: 200, data: data }).json(response.data);
+      console.log(response.data)
+      res.status(200).send({ result: 200, data: response.data })
+      .json(response.data);
     })
     .catch((err) => {
         console.log(err);
-        res.satus(500).send({ result: 500, error: err.message });
+        res.status(500).send({ result: 500, error: err.message });
       });
 };
 
@@ -27,4 +28,4 @@ const getAsoiaf = async (req, res) => {
 //     });
 // }
 
-module.exports = {getAsoiaf}
+module.exports = {getAsoiafs}
