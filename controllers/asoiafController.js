c76b3e7a235bc6b7b59efbdb322224f5820b93ca
DanielRenard <danieljuderenard@gmail.com>
2024-02-23 10:26:35 -0600

    api fetches and prints in client

--- controllers/asoiafController.js
+++ controllers/asoiafController.js
@@ -3,19 +3,16 @@ const Models = require("../models");
 
 const axios = require('axios')
 
+//"https://anapioficeandfire.com/api/houses/"
+
 const getAsoiafs = async (req, res) => {
     await axios
     .get("https://anapioficeandfire.com/api/houses/")
     .then((response) => {
-      console.log(response.data)
-      res.status(200).send({ result: 200, data: response.data })
-      .json(response.data);
+        console.log(response)
+        res.status(200).json(response.data)
     })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).send({ result: 500, error: err.message });
-      });
-};
+}
 
 // const getAsoiaf = (res) => {
 //   Models.Asoiaf.findAll({})
@@ -27,5 +24,7 @@ const getAsoiafs = async (req, res) => {
 //       res.satus(500).send({ result: 500, error: err.message });
 //     });
 // }
+
+// getAsoiafs()
 
 module.exports = {getAsoiafs}
